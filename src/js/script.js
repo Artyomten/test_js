e9bef563fb98d97b71217739b47ce2014373aa96
Artem <tenartyom2323ardi@gmail.com>
2026-09-22 12:17:41 +0500

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,67 @@
+// let a = 10;
+// console.log(a);
+
+// a=20;
+// console.log(a);
+
+// const rightDoor='blue';
+// console.log(rightDoor);
+
+// rightDoor='green';
+// console.log(rightDoor);
+
+// const user= {
+//     name: 'John',
+//     age: 30,
+//     isAdmin: true
+// };
+// console.log(user);
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.isAdmin);
+
+
+// +
+// -
+// /
+// %
+
+// =
+// ==
+// ===
+
+const shop={
+    name: 'TenShop',
+    since: 2025,
+    dept:'Shoes',
+    total_pr: 20.000,
+    is_open:true
+}
+console.log(shop);
+console.log(shop.name);
+console.log(shop.since);
+console.log(shop.dept);
+console.log(shop.total_pr);
+console.log(shop.is_open);
+
+
+const student = {
+    name:'Artem',
+    surname:'Ten',
+    age: 18, 
+    year: 2,
+    rating: 89.54
+}
+console.log(student);
+console.log(student.name,student.surname);
+console.log(student.rating);
+
+const company = {
+    name:'Yandex',
+    since: 1993,
+    total_pr: 1440000000000,
+    dept:'IT',
+    world_rating: 12,
+    is_working:true
+}
+console.log(company.name,company.since,company.total_pr,company.dept,company.world_rating,company.is_working);
